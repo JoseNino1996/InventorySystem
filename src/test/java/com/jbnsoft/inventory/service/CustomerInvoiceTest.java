d947a98b40d9120955ec7158a6fdd87e89b164bd
JoseNino1996 <jovenlee58@yahoo.com>
2018-10-08 10:10:23 +0800

    CustomerInvoice Test

---
+++ src/test/java/com/jbnsoft/inventory/service/CustomerInvoiceTest.java
@@ -0,0 +1,34 @@
+package com.jbnsoft.inventory.service;
+
+import com.jbnsoft.inventory.repository.customer.CustomerInvoice;
+import com.jbnsoft.inventory.sevice.customer.CustomerInvoiceService;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CustomerInvoiceTest {
+
+    private CustomerInvoiceService customerInvoiceService;
+
+    @Before
+    public  void setUp() {
+        customerInvoiceService = new CustomerInvoiceService();
+
+    }
+
+    @Test
+    public void paymentMustGreaterOrEqualSubtotal()  {
+        CustomerInvoice customerInvoice = new CustomerInvoice();
+
+        customerInvoice.setSubtotal(1000);
+        customerInvoice.setPayment(1100);
+
+        boolean result =   customerInvoiceService.checkCustomerPayment(customerInvoice);
+
+
+        Assert.assertEquals(true,result);
+
+
+    }
+
+}

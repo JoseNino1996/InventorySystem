4eadab943ff514a7fa09300cb23679c5cf0a3789
JoseNino1996 <jovenlee58@yahoo.com>
2018-10-16 01:44:00 +0800

    ..

--- src/main/java/com/jbnsoft/inventory/sevice/stock/ProductInventoryService.java
+++ src/main/java/com/jbnsoft/inventory/sevice/stock/ProductInventoryService.java
@@ -1,11 +1,14 @@
 package com.jbnsoft.inventory.sevice.stock;
+import com.jbnsoft.inventory.repository.customerinvoice.CustomerInvoice;
 import com.jbnsoft.inventory.repository.customerinvoice.ProductOrder;
 import com.jbnsoft.inventory.repository.customerinvoice.Transaction;
 import com.jbnsoft.inventory.repository.stock.ProductInventory;
 import com.jbnsoft.inventory.repository.stock.ProductInventoryRepository;
 import com.jbnsoft.inventory.repository.stock.StockLog;
+import com.jbnsoft.inventory.sevice.customerinvoice.CustomerInvoiceService;
 import com.jbnsoft.inventory.sevice.stock.helper.CreateOrder;
 import com.jbnsoft.inventory.sevice.stock.helper.DeleteOrder;
+import com.jbnsoft.inventory.sevice.stock.helper.ProcessOrder;
 import com.jbnsoft.inventory.sevice.stock.helper.UpdateOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,6 +19,8 @@ import java.util.*;
 public class ProductInventoryService implements IProductInventoryService {
     @Autowired
     private ProductInventoryRepository productInventoryRepository;
+    @Autowired
+    CustomerInvoiceService customerInvoiceService;
     @Autowired
     private CreateOrder createOrder;
     @Autowired
@@ -68,13 +73,12 @@ public class ProductInventoryService implements IProductInventoryService {
     }
 
     @Override
-    public void processOrderQuantity(List<ProductOrder> productOrders, String transactionType)  {
-
-
+    public void processOrderQuantity(CustomerInvoice customerInvoice)  {
+        List<ProductOrder> productOrders = customerInvoice.getProductOrderList();
+        String transactionType = customerInvoice.getTransactionType();
         List<ProductInventory> productInventoryList = findAll();
         Map<Long,ProductInventory> mappedProductInventory = ProductInventoryUtil.getMappedProductInventory(productInventoryList);
-
-        Map<Long, Long> mappedProductIdAndOrderedQuantity = getMappedProductIdAndOrderQuantity(productOrders,mappedProductInventory);
+        Map<Long, Long> mappedProductIdAndOrderedQuantity = ProductInventoryUtil.getMappedProductIdAndOrderQuantity(productOrders,mappedProductInventory);
 
         if(transactionType.equalsIgnoreCase(String.valueOf(Transaction.CREATE))) {
 
@@ -85,23 +89,20 @@ public class ProductInventoryService implements IProductInventoryService {
             deleteOrder.processOrderQuantity(mappedProductIdAndOrderedQuantity,mappedProductInventory);
 
         } else if (transactionType.equalsIgnoreCase(String.valueOf(Transaction.UPDATE))){
+            CustomerInvoice storedCustomerInvoice = customerInvoiceService.findById(customerInvoice.getId());
+
+            Map<Long,Long> mappedCurrentProductOrder = ProductInventoryUtil.getMappedProductIdAndOrderQuantity(storedCustomerInvoice.getProductOrderList(), mappedProductInventory);
+            updateOrder.setCurrentProductOrder(mappedCurrentProductOrder);
 
             updateOrder.processOrderQuantity(mappedProductIdAndOrderedQuantity,mappedProductInventory);
         }
 
     }
 
-    public Map<Long,Long> getMappedProductIdAndOrderQuantity(List<ProductOrder> productOrders , Map<Long,ProductInventory> mappedProductInventory) {
-        Map<Long, Long> mappedProductIdAndOrderedQuantity = new HashMap<>();
-        for (ProductOrder productOrder : productOrders) {
 
-            ProductInventory productInventory = mappedProductInventory.get(productOrder.getProduct().getId());
-             productOrder.setPrice(productInventory.getPrice());
 
-            mappedProductIdAndOrderedQuantity.put(productInventory.getId(), productOrder.getOrderedQty());
-        }
-        return  mappedProductIdAndOrderedQuantity;
-    }
+
+
 
 
     @Override
